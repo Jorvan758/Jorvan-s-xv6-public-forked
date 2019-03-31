b830a7b03d6dd2924d5ec7ce103130cb86d4bb28
Jorvan <jorgleon@alumnos.uai.cl>
2019-03-30 22:21:23 -0400

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -1,6 +1,14 @@
 // |J|
 
 //#include <stdlib.h>
+#include "types.h"
+#include "x86.h"
+#include "defs.h"
+#include "date.h"
+#include "param.h"
+#include "memlayout.h"
+#include "mmu.h"
+#include "proc.h"
 
 int main(void) {
     return 1; //printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
