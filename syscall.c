90366b8e3b6e39009ce6d2613e01d924dedb391a
Jorvan <jorgleon@alumnos.uai.cl>
2019-04-13 16:26:34 -0400

    Initial travis release

--- syscall.c
+++ syscall.c
@@ -6,7 +6,7 @@
 #include "proc.h"
 #include "x86.h"
 //#include "syscall.h"
-#include "syscall.c"
+//#include "syscall.c"
 
 // User code makes a system call with INT T_SYSCALL.
 // System call number in %eax.
