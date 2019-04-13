5d54d8423046439a3491ff45f3bf4d822c740c47
Jorvan <jorgleon@alumnos.uai.cl>
2019-04-13 17:31:53 -0400

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -1,20 +1,7 @@
-// |J|
-sudo apt-get install gcc-multilib
-sudo apt-get install g++-multilib
+
 #include <stdlib.h>
-#include <stdio.h>
-/*#include "types.h"
-#include "x86.h"
-#include "defs.h"
-#include "date.h"
-#include "param.h"
-#include "memlayout.h"
-#include "mmu.h"
-#include "proc.h"
-#include "syscall.c"*/
 
 int main(void) {
     printf("La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
-    //exit();
     return 1;
     }
