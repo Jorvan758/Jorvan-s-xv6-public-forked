7d2f27b1b4adb11b4ddf017e51ca665774099bd3
Jorvan <jorgleon@alumnos.uai.cl>
2019-04-14 13:47:26 -0400

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -6,17 +6,3 @@
     printf("Holi holi"); //La cantidad de procesos en ejecucion en la CPU es %i\n", getprocs());
     return 1;
     }*/
-
-
-
-#include "types.h"
-
-#include "stat.h"
-#include "user.h"
-
-
-int 
-main(void) {
-	printf(1,"El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
-	exit();
-	}
