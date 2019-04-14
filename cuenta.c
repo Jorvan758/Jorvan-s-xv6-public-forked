7126ccba9799980751e030794ba8bcc72062433f
Jorvan <jorgleon@alumnos.uai.cl>
2019-04-14 13:44:46 -0400

    Initial travis release

--- cuenta.c
+++ cuenta.c
@@ -12,16 +12,11 @@
 #include "types.h"
 
 #include "stat.h"
-
 #include "user.h"
-
 
 
 int 
 main(void) {
-
-    printf(1,"El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
-
-    exit();
-
-    }
+	printf(1,"El numero de procesos corriendo en el sistema es de: %d\n", getprocs());
+	exit();
+	}
