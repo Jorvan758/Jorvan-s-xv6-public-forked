931794f28d38873f0a91aa53fb94d1cb59b7203b
Jorvan <jorgleon@alumnos.uai.cl>
2019-05-30 18:37:43 -0400

    Initial travis release

--- direccionador.c
+++ direccionador.c
@@ -2,13 +2,11 @@
 #include "types.h"
 #include "stat.h"
 #include "user.h"
-//#include "stdio.h"
 
 int main(void) {
     char name[20];
     printf(1,"Hello. What's your name?\n");
     gets(name,20);
-    //fgets(name,20,stdin);
     printf(1,"Hi there, %s\n", name);
     printf(1,"%d\n", directioner());
     exit();
