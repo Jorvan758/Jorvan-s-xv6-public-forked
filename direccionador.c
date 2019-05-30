3ef26b380c1a71676de6e8d365827014368929b9
Jorvan <jorgleon@alumnos.uai.cl>
2019-05-30 17:24:49 -0400

    Initial travis release

---
+++ direccionador.c
@@ -0,0 +1,12 @@
+//Despliega los procesos "activos"
+#include "types.h"
+#include "stat.h"
+#include "user.h"
+
+int main(void) {
+    char name[20];
+    printf("Hello. What's your name?\n");
+    scanf("%d", &name);
+    printf("Hi there, %d", name);
+    exit();
+    }
