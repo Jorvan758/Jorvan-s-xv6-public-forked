4235344bfff7b64205aef8f001dc2366fab01cd7
Jorvan <jorgleon@alumnos.uai.cl>
2019-05-30 20:51:06 -0400

    Initial travis release

--- direccionador.c
+++ direccionador.c
@@ -7,11 +7,13 @@ int main(void) {
     char name[20];
     printf(1,"Hello. What's your name?\n");
     gets(name,20);
-    for(int a=1; a<20; a++){
+    int a;
+    for(a=1; a<20; a++){
         if ((name[a-1] == "\\") && (name[a] == "n")){
             name[a-1] = "";
             name[a] = "";
             break;
+            }
         }
     printf(1,"Hi there, %s\n", name);
     printf(1,"%d\n", directioner());
