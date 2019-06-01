ded475dd1793fde2e525319232c8d4c4f2045b20
Jorvan <jorgleon@alumnos.uai.cl>
2019-06-01 10:20:35 -0400

    Initial travis release

--- direccionador.c
+++ direccionador.c
@@ -19,59 +19,60 @@ int main(void) {
     if (name[10] != '\0'){
         exit();
         }
-    int dir_temp = 0;
-    for(a=2; a<11; a++){
-        if (name[a] == '0'){
-            dir_temp += 0;
+    else{
+        int dir_temp = 0;
+        for(a=2; a<11; a++){
+            if (name[a] == '0'){
+                dir_temp += 0;
+                }
+            else if (name[a] == '1'){
+                dir_temp += 1;
+                }
+            else if (name[a] == '2'){
+                dir_temp += 2;
+                }
+            else if (name[a] == '3'){
+                dir_temp += 3;
+                }
+            else if (name[a] == '4'){
+                dir_temp += 4;
+                }
+            else if (name[a] == '5'){
+                dir_temp += 5;
+                }
+            else if (name[a] == '6'){
+                dir_temp += 6;
+                }
+            else if (name[a] == '7'){
+                dir_temp += 7;
+                }
+            else if (name[a] == '8'){
+                dir_temp += 8;
+                }
+            else if (name[a] == '9'){
+                dir_temp += 9;
+                }
+            else if (name[a] == 'a'){
+                dir_temp += 10;
+                }
+            else if (name[a] == 'b'){
+                dir_temp += 11;
+                }
+            else if (name[a] == 'c'){
+                dir_temp += 12;
+                }
+            else if (name[a] == 'd'){
+                dir_temp += 13;
+                }
+            else if (name[a] == 'e'){
+                dir_temp += 14;
+                }
+            else if (name[a] == 'f'){
+                dir_temp += 15;
+                }
             }
-        else if (name[a] == '1'){
-            dir_temp += 1;
-            }
-        else if (name[a] == '2'){
-            dir_temp += 2;
-            }
-        else if (name[a] == '3'){
-            dir_temp += 3;
-            }
-        else if (name[a] == '4'){
-            dir_temp += 4;
-            }
-        else if (name[a] == '5'){
-            dir_temp += 5;
-            }
-        else if (name[a] == '6'){
-            dir_temp += 6;
-            }
-        else if (name[a] == '7'){
-            dir_temp += 7;
-            }
-        else if (name[a] == '8'){
-            dir_temp += 8;
-            }
-        else if (name[a] == '9'){
-            dir_temp += 9;
-            }
-        else if (name[a] == 'a'){
-            dir_temp += 10;
-            }
-        else if (name[a] == 'b'){
-            dir_temp += 11;
-            }
-        else if (name[a] == 'c'){
-            dir_temp += 12;
-            }
-        else if (name[a] == 'd'){
-            dir_temp += 13;
-            }
-        else if (name[a] == 'e'){
-            dir_temp += 14;
-            }
-        else if (name[a] == 'f'){
-            dir_temp += 15;
-            }
+        printf(1,"La direccion fisica es: %d\n", dir_temp);
         }
-    printf(1,"La direccion fisica es: %d\n", dir_temp);
-
     printf(1,"%d\n", directioner());
 
     /*char *virtual_addr = name;
