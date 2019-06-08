05cdec35fbf40b9345e8eee3bef49ceb0d01be4d
Jorvan <jorgleon@alumnos.uai.cl>
2019-06-08 16:41:12 -0400

    Initial travis release

--- sysproc.c
+++ sysproc.c
@@ -93,7 +93,7 @@ sys_directioner(char* name)
   /*pde_t *pgdir,*pgtab,*pde;
   pte = &pgtab[PTX(virtual_address)];
   pgtab = (pte_t*)V2P(PTE_ADDR(*pde));*/
-  int physical_address=(char*)P2V(PTE_ADDR(*pte));
+  *int physical_address=(char*)P2V(PTE_ADDR(*pte));
   /*uint a = PGROUNDDOWN(rcr2()); // round down faulty VA to page start
   char *mem;
   mem = kalloc();
@@ -118,7 +118,7 @@ sys_directioner(char* name)
     a += PGSIZE;
     pa += PGSIZE;
     }*/
-  cprintf("%d\n", physical_address);
+  cprintf("%p\n", (void*)physical_address);
   /*char name[20];
   //gets(name,20);
   int a;
